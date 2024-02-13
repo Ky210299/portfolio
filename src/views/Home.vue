311147d8eed51e1e7770ba0f113c5ec9dee50dbd
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-02-13 17:02:23 -0500

    adding perfile component and render adding main and section tag on home view;

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <section>
+
+    </section>
+    <section>
+      
+    </section>
+  </main>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
