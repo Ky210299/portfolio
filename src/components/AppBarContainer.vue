38570851e53f1a61dc4cc195357f152b46821eb9
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-02-15 11:01:26 -0500

    adding AppBarContainer

---
+++ src/components/AppBarContainer.vue
@@ -0,0 +1,30 @@
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+
+})
+
+</script>
+ 
+ ««««««««««««««««««««--Template--»»»»»»»»»»»»»»»»»»»»»
+
+<template>
+  <nav class="nav-filter-container">
+    Nav filter container
+  </nav>
+</template>
+ 
+ ««««««««««««««««««««--Style--»»»»»»»»»»»»»»»»»»»»»
+
+<style>
+.nav-filter-container {
+  width: 90%;
+  height: 25px;
+  margin: auto;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding: 5px;
+  border-radius: 10px;
+}
+</style>
