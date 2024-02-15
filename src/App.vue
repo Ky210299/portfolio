25c2fd60be4183a60bad089ff0c4c2501d8f2951
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-02-15 11:12:49 -0500

    adding components to app.vue and renaming "pages" folder to "view" folder

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <NavegationContainer />
   <PerfileContainer />
+  <AppBarContainer />
 </template>
 
 <script lang="ts" setup>
 import NavegationContainer from './components/NavegationContainer.vue';
 import PerfileContainer from './components/PerfileContainer.vue';
+import AppBarContainer from './components/AppBarContainer.vue';
 </script>
 
 <style></style>
