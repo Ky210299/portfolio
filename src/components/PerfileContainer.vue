a4737f3e2537229a3f764cbb577f6144db113bbe
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-02-15 09:06:44 -0500

    modifying perfile container layout

---
+++ src/components/PerfileContainer.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+export default defineComponent({
+
+})
+
+</script>
+ 
+ 
+<template>
+  <div class="perfile-container">
+    <p>Perfile Container</p>
+  </div>
+</template>
+ 
+ 
+<style>
+.perfile-container {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  margin-top: 10px;
+  margin-bottom: 25px;
+  padding-top: 2px;
+  border-radius: 10px;
+}
+</style>
