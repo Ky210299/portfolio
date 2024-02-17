9c5300b04387b77cabe2c57c35838d77ade3c1f3
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-02-17 15:59:48 -0500

    before reinstall OS

---
+++ src/components/DropDownMenu.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+
+})
+
+
+</script>
+ 
+ ««««««««««««««««««««--Template--»»»»»»»»»»»»»»»»»»»»»
+
+<template>
+    <div>
+        <button>menu</button>
+        
+    </div>
+</template>
+
+ 
+ ««««««««««««««««««««--Style--»»»»»»»»»»»»»»»»»»»»»
+
+<style></style>
