cbc46f09568dc333900b8b473e8f3ebe09d9ec61
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-02-19 23:19:48 -0500

    Adding socials networks and remodeling profile view

--- src/components/ProfileContainer.vue
+++ src/components/ProfileContainer.vue
@@ -7,11 +7,11 @@
 <template>
 	<section class="profile-section" id="home">
 		<div class="profile-section-text">
-			<h2 class="h2"><em>Hi! I'm Leonardo Bazan Marquez</em></h2>
-			<h3 class="h3"><strong>Frontend Developep</strong></h3>
+			<h2 class="h2">Hi! I'm Leonardo Bazan Marquez</h2>
+			<h3 class="h3"><strong class="strong">Frontend Developep</strong></h3>
 			<BarSocials />
 		</div>
-		<div class="imagen"> Algo bonito aqui</div>
+		<div class="imagen">Algo bonito aqui</div>
 	</section>
 </template>
 
