addd2d4e724f0baa96e757411ff4da4cae69f4bf
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-02-19 21:08:06 -0500

    fixing text

--- src/components/ProfileContainer.vue
+++ src/components/ProfileContainer.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-	<section class="profile-section">
+	<section class="profile-section" id="home">
 		<div class="profile-section-text">
 			<h2 class="h2"><em>Hi! I'm Leonardo Bazan Marquez</em></h2>
 			<h3 class="h3"><strong>Frontend Developep</strong></h3>
