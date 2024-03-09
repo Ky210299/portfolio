de97a3accbed0efe78702be972f22b75fbf51ed2
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-03-09 18:34:29 -0500

    I found the preview script

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: "https://ky210299.github.io/Ky210299/vite-deploy/",
+	base: "/Ky210299/",
 });
