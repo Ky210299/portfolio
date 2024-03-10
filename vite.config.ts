cc06e372ab7f0ce8fb7249568926e15923ff2e19
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-03-09 19:33:50 -0500

    removing base on vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: "https://ky210299.github.io/Ky210299/vite-deploy/",
 });
