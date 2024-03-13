23decf3516b4169567d44571e04fc1d66085c212
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-03-13 12:07:35 -0400

    adding form html

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -10,6 +10,20 @@ export default defineComponent({
 
 <template>
 
+    <form id="contactForm" action="" autocomplete="on" method="post"
+        class="Contact">
+        <label class="-label" for="formName">What's your name?</label>
+        <input class="-input" type="text" id="formName">
+        <label class="-label" for="formEmail">What's your email?</label>
+        <input class="-input" type="email" id="formEmail">
+        <label class="-label" for="formMessage"></label>
+        <textarea class="-input" form="contactForm" id="formMessage"
+            autocomplete="on" cols="30" rows="10" maxlength="2000"
+            required></textarea>
+        <output class="-output" form="contactForm"
+            for="formName formEmail formMessage"></output>
+        <button class="-submit" type="submit">Submit</button>
+    </form>
 </template>
 
 ««««««««««««««««««««--Style--»»»»»»»»»»»»»»»»»»»»»
