2fcddf8957d4be34527c65fc7e1d4ac1edebc905
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-03-23 07:57:10 -0400

    moving the layout style from main.css to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-	<div ref="test"  class="scroll">
-		<HeaderContainer  />
+	<div ref="test" class="scroll">
+		<HeaderContainer />
 		<Home id="home" />
 		<main class="main" id="projects">
-			<Projects  />
+			<Projects />
 		</main>
-		<Contact/>
-		<About />	
+		<Contact class="contact-section" />
+		<About />
 	</div>
 </template>
 
 <script lang="ts" setup>
-	
-	import HeaderContainer from "./components/HeaderContainer.vue";
-	import Home from "./views/Home.vue";
-	import Projects from "./views/Projects.vue";
-	import Contact from "./views/Contact.vue";
-	import About from "./views/About.vue";
+
+import HeaderContainer from "./components/HeaderContainer.vue";
+import Home from "./views/Home.vue";
+import Projects from "./views/Projects.vue";
+import Contact from "./views/Contact.vue";
+import About from "./views/About.vue";
 
 
-	
+
 </script>
-<style scoped>
-	
+<style>
+.header {
+	.nav {
+		flex-grow: 1;
+	}
+
+	.h1 {
+		flex-shrink: 1;
+		justify-self: flex-start;
+	}
+
+	.header-menu {
+		flex-shrink: 0;
+		justify-self: flex-end;
+	}
+}
+
+.profile-section {
+	width: 90vw;
+	display: flex;
+	min-height: 100dvh;
+	margin: auto;
+
+}
+
+.main {
+	width: 90vw;
+	margin: auto;
+	display: flex;
+	flex-flow: column wrap;
+	justify-content: space-around;
+	align-items: center;
+	min-height: 100dvh;
+	padding: 2rem;
+	padding-top: 3rem;
+
+}
+
+.about-section {
+	margin: auto;
+	min-height: 100dvh;
+	padding: 2rem;
+}
+
+.contact-section {
+	width: 90vw;
+	min-height: 100dvh;
+	margin: auto;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	flex-flow: column nowrap;
+	padding-inline: 2rem;
+	padding-block: 4rem;
+
+}
+
+.footer {
+	height: 5dvh;
+}
 </style>
